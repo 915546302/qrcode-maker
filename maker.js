e25db824f0ebc091dda6493ba5ed1e1132c17217
915546302 <915546302@163.com>
2014-01-20 00:11:05 +0800

    modified:   maker.js

--- maker.js
+++ maker.js
@@ -1,59 +1,48 @@
 //2014-1-19
 //connect: 915546302@163.com
-var draw_qrcode = function(text, typeNumber, errorCorrectLevel) {
-  document.write(create_qrcode(text, typeNumber, errorCorrectLevel) );
-};
-
-var create_qrcode = function(text, typeNumber, errorCorrectLevel, table) {
-
-  var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M');
-  qr.addData(text);
-  qr.make();
-  return qr.createImgTag();
-};
-var update_qrcode = function(t) {
-  var text = t.replace(/^[\s\u3000]+|[\s\u3000]+$/g, '');
-  return create_qrcode(text);
-};
-var update_qrcode_no = function() {
-  var text = document.getElementById("divinput").value.replace(/^[\s\u3000]+|[\s\u3000]+$/g, '');
-  return create_qrcode(text);
-
-};
+function qrcode_make(){
+  var divimage = document.getElementById("divimage");
+  document.getElementById("qrcode").remove();
+  var qrcode1=document.createElement('div');
+  qrcode1.setAttribute('id','qrcode');
+  divimage.appendChild(qrcode1);
+  var textv = document.getElementById("divinput").value;
+  new QRCode(document.getElementById("qrcode"), {
+          text: textv,
+          width: 170,
+          height: 170,
+          colorDark : "#000000",
+          colorLight : "#ffffff",
+          correctLevel : QRCode.CorrectLevel.H
+  });
+}
 var Generator = {
   request : function(){
-    var settime=(new Date).getTime()-2*3600*1000;
-    chrome.history.search({text:'',startTime:settime},
-      function(historyItems){
+
+    chrome.tabs.getSelected(null,function(tab){
         var title=document.getElementById("title");
         var h3=document.createElement("h3");
         h3.innerHTML="二维码maker";
         title.appendChild(h3);
 
         var inputtext=document.getElementById("divinput");   
-        inputtext.value=historyItems[0].url;        
-
-        var divimage = document.getElementById("divimage");
-        var div=document.createElement('div');
-        div.setAttribute('id','imgsrc');
-        div.innerHTML=update_qrcode(historyItems[0].url);
-        divimage.appendChild(div);
-      }
-    );
-  },
-  makebutton:  function(){
-    var imgsrc= document.getElementById("imgsrc");
-    var divimage = document.getElementById("divimage");
-    divimage.removeChild(imgsrc);
-    var div=document.createElement('div');
-    div.setAttribute('id','imgsrc');
-    div.innerHTML=update_qrcode_no();
-    divimage.appendChild(div);
+        inputtext.value=tab.url;
+        new QRCode(document.getElementById("qrcode"), {
+          text: tab.url,
+          width: 170,
+          height: 170,
+          colorDark : "#000000",
+          colorLight : "#ffffff",
+          correctLevel : QRCode.CorrectLevel.H
+        });
+    });
+        
   }
 }
+
 document.addEventListener('DOMContentLoaded', function () {
   Generator.request();
-});
-document.addEventListener('click',function(){
-  Generator.makebutton();
+  document.querySelector('#makebutton').addEventListener('click',function(){
+    qrcode_make();
+  });
 });
